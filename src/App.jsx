09dc051bfd5ba5fda16b995c9ea3fe09b7093279
shagan sandhu <shagan1298.be21@chitkara.edu.in>
2024-09-04 15:40:42 +0530

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,426 @@
+import "./App.css";
+import { useEffect, useMemo, useState } from "react";
+import Start from "./components/Start";
+import Timer from "./components/Timer";
+import Trivia from "./components/Trivia";
+
+function App() {
+  const [username, setUsername] = useState(null);
+  const [timeOut, setTimeOut] = useState(false);
+  const [questionNumber, setQuestionNumber] = useState(1);
+  const [earned, setEarned] = useState("$ 0");
+
+  const data = [
+    {
+      id: 1,
+      question: "Rolex is a company that specializes in what type of product?",
+      answers: [
+        {
+          text: "Phone",
+          correct: false,
+        },
+        {
+          text: "Watches",
+          correct: true,
+        },
+        {
+          text: "Food",
+          correct: false,
+        },
+        {
+          text: "Cosmetic",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 2,
+      question: "When did the website `Facebook` launch?",
+      answers: [
+        {
+          text: "2004",
+          correct: true,
+        },
+        {
+          text: "2005",
+          correct: false,
+        },
+        {
+          text: "2006",
+          correct: false,
+        },
+        {
+          text: "2007",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 3,
+      question: "Who played the character of harry potter in movie?",
+      answers: [
+        {
+          text: "Johnny Deep",
+          correct: false,
+        },
+        {
+          text: "Leonardo Di Caprio",
+          correct: false,
+        },
+        {
+          text: "Denzel Washington",
+          correct: false,
+        },
+        {
+          text: "Daniel Red Cliff",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 4,
+      question: "Which god is also known as ‘Gauri Nandan’?",
+      answers: [
+        {
+          text: "Agni",
+          correct: false,
+        },
+        {
+          text: "Indra",
+          correct: false,
+        },
+        {
+          text: "Hunuman",
+          correct: false,
+        },
+        {
+          text: "Ganesha",
+          correct: true,
+        },
+      ],
+    },
+    
+    {
+      id: 5,
+      question: "What does not grow on tree according to a popular Hindi saying?",
+      answers: [
+        {
+          text: "Leaves",
+          correct: false,
+        },
+        {
+          text: "Flower",
+          correct: false,
+        },
+        {
+          text: "Fruits",
+          correct: false,
+        },
+        {
+          text: "Money",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 6,
+      question: "What does not grow on tree according to a popular Hindi saying?",
+      answers: [
+        {
+          text: "Leaves",
+          correct: false,
+        },
+        {
+          text: "Flower",
+          correct: false,
+        },
+        {
+          text: "Fruits",
+          correct: false,
+        },
+        {
+          text: "Money",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 7,
+      question: "What does not grow on tree according to a popular Hindi saying?",
+      answers: [
+        {
+          text: "Leaves",
+          correct: false,
+        },
+        {
+          text: "Flower",
+          correct: false,
+        },
+        {
+          text: "Fruits",
+          correct: false,
+        },
+        {
+          text: "Money",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 8,
+      question: "What does not grow on tree according to a popular Hindi saying?",
+      answers: [
+        {
+          text: "Leaves",
+          correct: false,
+        },
+        {
+          text: "Flower",
+          correct: false,
+        },
+        {
+          text: "Fruits",
+          correct: false,
+        },
+        {
+          text: "Money",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 9,
+      question: "What does not grow on tree according to a popular Hindi saying?",
+      answers: [
+        {
+          text: "Leaves",
+          correct: false,
+        },
+        {
+          text: "Flower",
+          correct: false,
+        },
+        {
+          text: "Fruits",
+          correct: false,
+        },
+        {
+          text: "Money",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 10,
+      question: "What does not grow on tree according to a popular Hindi saying?",
+      answers: [
+        {
+          text: "Leaves",
+          correct: false,
+        },
+        {
+          text: "Flower",
+          correct: false,
+        },
+        {
+          text: "Fruits",
+          correct: false,
+        },
+        {
+          text: "Money",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 11,
+      question: "What does not grow on tree according to a popular Hindi saying?",
+      answers: [
+        {
+          text: "Leaves",
+          correct: false,
+        },
+        {
+          text: "Flower",
+          correct: false,
+        },
+        {
+          text: "Fruits",
+          correct: false,
+        },
+        {
+          text: "Money",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 12,
+      question: "What does not grow on tree according to a popular Hindi saying?",
+      answers: [
+        {
+          text: "Leaves",
+          correct: false,
+        },
+        {
+          text: "Flower",
+          correct: false,
+        },
+        {
+          text: "Fruits",
+          correct: false,
+        },
+        {
+          text: "Money",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 13,
+      question: "What does not grow on tree according to a popular Hindi saying?",
+      answers: [
+        {
+          text: "Leaves",
+          correct: false,
+        },
+        {
+          text: "Flower",
+          correct: false,
+        },
+        {
+          text: "Fruits",
+          correct: false,
+        },
+        {
+          text: "Money",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 14,
+      question: "What does not grow on tree according to a popular Hindi saying?",
+      answers: [
+        {
+          text: "Leaves",
+          correct: false,
+        },
+        {
+          text: "Flower",
+          correct: false,
+        },
+        {
+          text: "Fruits",
+          correct: false,
+        },
+        {
+          text: "Money",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 15,
+      question: "What does not grow on tree according to a popular Hindi saying?",
+      answers: [
+        {
+          text: "Leaves",
+          correct: false,
+        },
+        {
+          text: "Flower",
+          correct: false,
+        },
+        {
+          text: "Fruits",
+          correct: false,
+        },
+        {
+          text: "Money",
+          correct: true,
+        },
+      ],
+    },
+  ];
+
+  const moneyPyramid = useMemo(
+    () =>
+      [
+        { id: 1, amount: "$ 100" },
+        { id: 2, amount: "$ 200" },
+        { id: 3, amount: "$ 300" },
+        { id: 4, amount: "$ 500" },
+        { id: 5, amount: "$ 1.000" },
+        { id: 6, amount: "$ 2.000" },
+        { id: 7, amount: "$ 4.000" },
+        { id: 8, amount: "$ 8.000" },
+        { id: 9, amount: "$ 16.000" },
+        { id: 10, amount: "$ 32.000" },
+        { id: 11, amount: "$ 64.000" },
+        { id: 12, amount: "$ 125.000" },
+        { id: 13, amount: "$ 250.000" },
+        { id: 14, amount: "$ 500.000" },
+        { id: 15, amount: "$ 1.000.000" },
+      ].reverse(),
+    []
+  );
+
+  useEffect(() => {
+    questionNumber > 1 &&
+      setEarned(moneyPyramid.find((m) => m.id === questionNumber - 1).amount);
+  }, [questionNumber, moneyPyramid]);
+
+  return (
+    <div className="app">
+      {!username ? (
+        <Start setUsername={setUsername} />
+      ) : (
+        <>
+          <div className="main">
+            {timeOut ? (
+              <h1 className="endText">You earned: {earned}</h1>
+            ) : (
+              <>
+                <div className="top">
+                  <div className="timer">
+                    <Timer
+                      setTimeOut={setTimeOut}
+                      questionNumber={questionNumber}
+                    />
+                  </div>
+                </div>
+                <div className="bottom">
+                  <Trivia
+                    data={data}
+                    questionNumber={questionNumber}
+                    setQuestionNumber={setQuestionNumber}
+                    setTimeOut={setTimeOut}
+                  />
+                </div>
+              </>
+            )}
+          </div>
+          <div className="pyramid">
+            <ul className="moneyList">
+              {moneyPyramid.map((m) => (
+                <li
+                  className={
+                    questionNumber === m.id
+                      ? "moneyListItem active"
+                      : "moneyListItem"
+                  }
+                >
+                  <span className="moneyListItemNumber">{m.id}</span>
+                  <span className="moneyListItemAmount">{m.amount}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
